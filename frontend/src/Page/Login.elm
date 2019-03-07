d5072ccb641ce410ac7d227c5f83abd3941de828
Albert Nigmatzianov <albertnigma@gmail.com>
2019-03-07 14:58:23 +0500

    Add API interaction to frontend

--- frontend/src/Page/Login.elm
+++ frontend/src/Page/Login.elm
@@ -5,6 +5,8 @@ import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Http
+import Json.Encode as Encode
 import Skeleton
 
 
@@ -13,12 +15,12 @@ import Skeleton
 
 
 type alias Model =
-    { login : String, password : String, isRegistering : Bool }
+    { username : String, password : String, isRegistering : Bool }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { login = "", password = "", isRegistering = False }, Cmd.none )
+    ( { username = "", password = "", isRegistering = False }, Cmd.none )
 
 
 
@@ -46,8 +48,8 @@ view model =
     , children =
         [ h1 [] [ text "Tic-Tac-Toe" ]
         , h2 [] [ text title ]
-        , input [ class "mb-8", onInput EnteredLogin, placeholder "Nickname", value model.login ] []
-        , input [ class "mb-8", onInput EnteredPassword, placeholder "Passwort", value model.password ] []
+        , input [ class "mb-8", onInput EnteredUsername, placeholder "Nickname", value model.username ] []
+        , input [ class "mb-8", onInput EnteredPassword, placeholder "Passwort", type_ "password", value model.password ] []
         , button [ class "mb-16", onClick ClickedSubmit ] [ text "Go" ]
         , bottomText
         ]
@@ -59,27 +61,44 @@ view model =
 
 
 type Msg
-    = EnteredLogin String
+    = EnteredUsername String
     | EnteredPassword String
     | ClickedSubmit
     | ClickedGoToRegister
     | ClickedGoToLogin
+    | LoggedIn (Result Http.Error String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        EnteredLogin login ->
-            ( { model | login = login }, Cmd.none )
+        EnteredUsername username ->
+            ( { model | username = username }, Cmd.none )
 
         EnteredPassword password ->
             ( { model | password = password }, Cmd.none )
 
         ClickedSubmit ->
-            ( model, Cmd.none )
+            ( model, Http.post { url = "http://localhost:3000/api/user/new", body = Http.jsonBody (loginEncoder model), expect = Http.expectString LoggedIn } )
 
         ClickedGoToRegister ->
             ( { model | isRegistering = True }, Cmd.none )
 
         ClickedGoToLogin ->
             ( { model | isRegistering = False }, Cmd.none )
+
+        LoggedIn result ->
+            case result of
+                Ok username ->
+                    ( { model | username = username }, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
+
+
+loginEncoder : Model -> Encode.Value
+loginEncoder model =
+    Encode.object
+        [ ( "username", Encode.string model.username )
+        , ( "password", Encode.string model.password )
+        ]
