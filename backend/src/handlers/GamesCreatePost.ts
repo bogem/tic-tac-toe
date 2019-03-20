7968097653da7fc69138dc35b7b99c3d205f9446
Albert Nigmatzianov <albertnigma@gmail.com>
2019-03-20 19:03:08 +0100

    backend: Update GamesCreatePost

--- backend/src/handlers/GamesCreatePost.ts
+++ backend/src/handlers/GamesCreatePost.ts
@@ -1,10 +1,9 @@
 import { RequestHandler } from "express";
 import uuid = require("uuid/v4");
 
-import { GamesCreatePostRequestBody } from "common/types/api/games/create/post/RequestBody";
-import { docClient } from "../db/Db";
+import { GamesCreatePostRequestBody } from "../../../common/types/api/games/create/post/RequestBody";
 import { getUsernameWithToken } from "../utils/Tokens";
-import { Game } from "common/types/game";
+import { Game, GameStatusType } from "../../../common/types/game";
 import { put, putGamesHistoriesItem } from "../db/Fns";
 
 export const GamesCreatePostHandler: RequestHandler = async (req, res) => {
@@ -19,44 +18,19 @@ export const GamesCreatePostHandler: RequestHandler = async (req, res) => {
     try {
         // Put game.
         const gameId = uuid();
-        const creatorUsername = await getUsernameWithToken(token);
-        await putGame({ id: gameId, creatorUsername, ...body });
+        const hostUsername = await getUsernameWithToken(token);
+        const statusType = GameStatusType.WaitingForGuestJoin;
+        await putGame({ statusType, hostUsername, id: gameId, ...body });
 
         // Put game's history item.
-        const gamesHistoriesItemId = uuid();
         await putGamesHistoriesItem({
             gameId,
-            id: gamesHistoriesItemId,
-            status: { type: "WAITING_FOR_OPPONENT_JOIN" },
+            id: uuid(),
+            status: { type: statusType },
         });
-
-        // Add game's history item ID to game.
-        await addGamesHistoriesItemToGame(gamesHistoriesItemId, gameId);
 
         res.sendStatus(200);
     } catch (e) {}
 };
 
 const putGame = (game: Game): Promise<void> => put({ TableName: "Games", Item: game });
-
-const addGamesHistoriesItemToGame = (gamesHistoriesItemId: string, gameId: string): Promise<void> => {
-    const params = {
-        TableName: "Games",
-        Key: { id: gameId },
-        UpdateExpression: "set lastHistoryItemId=:id",
-        ExpressionAttributeValues: {
-            ":id": gamesHistoriesItemId,
-        },
-        ReturnValues: "NONE",
-    };
-
-    return new Promise((resolve, reject) => {
-        docClient.update(params, err => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve();
-            }
-        });
-    });
-};
