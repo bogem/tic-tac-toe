1f3a772054ac9721718f8b9c150b8d971a18e6bb
Albert Nigmatzianov <albertnigma@gmail.com>
2019-03-24 20:32:33 +0100

    backend: Add deleteToken

--- backend/src/models/Token.ts
+++ backend/src/models/Token.ts
@@ -1,9 +1,16 @@
 import uuid = require("uuid/v4");
 
 import { docClient } from "../db/Db";
-import { put } from "../db/Fns";
+import { put, onlyErrDocClientCallback } from "../db/Fns";
 
 export const ERROR_NO_USERNAME_FOUND = new Error("No username found");
+
+// DELETEs
+
+export const deleteToken = (token: string): Promise<void> =>
+    new Promise((resolve, reject) => {
+        docClient.delete({ TableName: "Tokens", Key: { token } }, onlyErrDocClientCallback(resolve, reject));
+    });
 
 // GETs
 
