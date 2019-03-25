82b048e2a186f9129070a4fd2f1e30f0cef322db
Albert Nigmatzianov <albertnigma@gmail.com>
2019-03-25 21:30:22 +0100

    frontend, package.json: Delete terser-webpack-plugin

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -2,21 +2,11 @@ const common = require("./webpack.common.js");
 const merge = require("webpack-merge");
 const { DefinePlugin, HashedModuleIdsPlugin } = require("webpack");
 const CompressionPlugin = require("compression-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
 
     optimization: {
-        minimizer: [
-            new TerserPlugin({
-                cache: false,
-                parallel: true,
-                terserOptions: {
-                    ecma: 6,
-                },
-            }),
-        ],
         splitChunks: {
             chunks: "all",
         },
